87480a4850283d1a1bae74a88a9f5415a348f78e
damionjestem <ndnd@protonmail.ch>
2019-07-14 01:44:30 +0200

    change admin page

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Item
 
 # Register your models here.
+
+
+class ItemAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Title/Date', {'fields': ['item_title', 'item_published']}),
+        ('Content', {'fields': ['item_content']})
+    ]
+
+
+admin.site.register(Item)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Item(models.Model):
+    item_title = models.CharField('Tytuł obiektu', max_length=200)
+    item_content = models.TextField('Treść obiektu')
+    item_publisher = models.DateTimeField('Opublikowano')
+
+    def __str__(self):
+        return self.item_title
