5b9bd387daa5d27b28fbfe4cc642813e4ed2917b
damionjestem <ndnd@protonmail.ch>
2019-07-14 20:02:42 +0200

    components of site as templates

--- main/views.py
+++ main/views.py
@@ -1,8 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Item
 
 # Create your views here.
 
 
 def homepage(request):
-    return HttpResponse("Damian Lesiewicz damian.lesiewicz@protonmail.com")
+    return render(request,
+                  "main/home.html",
+                  {"items": Item.objects.all})
